09d704838a6daa3d882ac928ebe483dd707252db
JurgisGuscia <j.d.guscia@gmail.com>
2025-08-30 15:26:52 +0300

    Installed a bramus router. Added router for all controllers @ index.php

---
+++ egzoticsos_backend/public/index.php
@@ -0,0 +1,77 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+use Dotenv\Dotenv;
+require "../config/connectToDatabase.php";
+
+require_once __DIR__ . '/../src/Controllers/VaccineTableController.php';
+require_once __DIR__ . '/../src/Controllers/ClassTableController.php';
+require_once __DIR__ . '/../src/Controllers/AnimalTableController.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
+$animalController = new AnimalTableController($pdo, $_ENV['ANIMAL_TABLE']);
+$classController = new ClassTableController($pdo, $_ENV['CLASS_TABLE']);
+$vaccineController = new VaccineTableController($pdo, $_ENV['VACCINE_TABLE']);
+
+$router = new \Bramus\Router\Router();
+
+
+# animal routes
+$router->get('/animals', fn() => $animalController->getAll());
+
+$router->get("/animals/(\d+)", fn($id) => $animalController->get((int)$id));
+
+$router->post('/animals', function() use ($animalController) {
+    $data = json_decode(file_get_contents('php://input'), true); 
+    $animalController->add($data);
+});
+
+$router->delete("/animals/(\d+)", fn($id) => $animalController->delete((int)$id));
+
+$router->put('/animals/(\d+)', function($id) use ($animalController) {
+    $data = json_decode(file_get_contents('php://input'), true); 
+    $animalController->edit((int)$id, $data);
+});
+
+
+# vaccine routes
+$router->get('/vaccines', fn() => $vaccineController->getAll());
+
+$router->get("/vaccines/(\d+)", fn($id) => $vaccineController->get((int)$id));
+
+$router->post('/vaccines', function() use ($vaccineController) {
+    $data = json_decode(file_get_contents('php://input'), true); 
+    $vaccineController->add($data);
+});
+
+$router->delete("/vaccines/(\d+)", fn($id) => $vaccineController->delete((int)$id));
+
+$router->put('/vaccines/(\d+)', function($id) use ($vaccineController) {
+    $data = json_decode(file_get_contents('php://input'), true); 
+    $vaccineController->edit((int)$id, $data);
+});
+
+
+# class routes
+$router->get('/classes', fn() => $classController->getAll());
+
+$router->get("/classes/(\d+)", fn($id) => $classController->get((int)$id));
+
+$router->post('/classes', function() use ($classController) {
+    $data = json_decode(file_get_contents('php://input'), true); 
+    $classController->add($data);
+});
+
+$router->delete("/classes/(\d+)", fn($id) => $classController->delete((int)$id));
+
+$router->put('/classes/(\d+)', function($id) use ($classController) {
+    $data = json_decode(file_get_contents('php://input'), true); 
+    $classController->edit((int)$id, $data);
+});
+
+
+$router->run();
+
+?>
